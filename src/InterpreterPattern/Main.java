a59d4098cf1cf15df6f23d8d08e16684c7a5ccf3
Lucas Gaspar <lucasprgaspar@gmail.com>
2019-06-06 10:27:25 -0300

    Interpreter Pattern

---
+++ src/InterpreterPattern/Main.java
@@ -0,0 +1,29 @@
+package InterpreterPattern;
+
+public class Main {
+
+	//Rule: Robert and John are Male
+	public static Expression getMaleExpression() {
+		Expression robert = new TerminalExpression("Robert");
+		Expression john = new TerminalExpression("John");
+		return new OrExpression(robert, john);
+	}
+
+	//Rule: Julie is a married women
+	public static Expression getMarriedWomanExpression() {
+		Expression julie = new TerminalExpression("Julie");
+		Expression married = new TerminalExpression("Married");
+		return new AndExpression(julie, married);
+	}
+
+
+	public static void main(String[] args) {
+		Expression isMale = getMaleExpression();
+		Expression isMarriedWomam = getMarriedWomanExpression();
+
+		System.out.println("John is male? " + isMale.interpret("John"));
+		System.out.println("Julie is a married women? " + isMarriedWomam.interpret("Married Julie"));
+
+	}
+
+}
