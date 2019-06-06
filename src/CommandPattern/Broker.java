63aca87ff3858270e4225f777af37551907ca082
Lucas Gaspar <lucasprgaspar@gmail.com>
2019-06-06 10:12:01 -0300

    Command Pattern

---
+++ src/CommandPattern/Broker.java
@@ -0,0 +1,18 @@
+package CommandPattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Broker {
+	
+	private List<Order> orderList = new ArrayList<Order>();
+	
+	public void takeOrder(Order order) {
+		orderList.add(order);
+	}
+	
+	public void placeOrders() {
+		orderList.forEach(order->order.execute());
+		orderList.clear();
+	}
+}
